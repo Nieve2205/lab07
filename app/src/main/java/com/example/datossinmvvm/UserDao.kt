8bf7410e6da66e3d2001ad7d740eaea448477d18
Nieve2205 <ashleepre2022@gmail.com>
2024-09-16 15:44:45 -0500

    Se creo una nueva interfaz

---
+++ app/src/main/java/com/example/datossinmvvm/UserDao.kt
@@ -0,0 +1,15 @@
+package com.example.datossinmvvm
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM User")
+    suspend fun getAll(): List<User>
+
+    @Insert
+    suspend fun insert(user: User)
+}
+
